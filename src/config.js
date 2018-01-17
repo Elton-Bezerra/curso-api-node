e6156116b4c2b30c7668b611d002e412cfc44214
Elton-Bezerra <elton.cin@hotmail.com>
2018-01-17 10:11:05 -0200

    Delete config.js

--- src/config.js
+++
@@ -1,7 +0,0 @@
-global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
-global.EMAIL_TMPL = 'Rérou,<strong> {0}, bem vindo ao Node Store!</strong>';
-module.exports = {
-    connectionString: 'mongodb://elton:elton@ds046367.mlab.com:46367/nodestore',
-    sendgridKey: '<APIKEY>',
-    containerConnectionString: 'TBD'
-}
